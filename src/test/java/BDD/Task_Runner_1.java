866e9d7090d649293ff955bfffd16a9afd4d0e1e
shaik afroz <45727962+SHAIKAFROZ-byte@users.noreply.github.com>
2024-02-22 13:47:48 +0530

    Update Task_Runner_1.java

--- src/test/java/BDD/Task_Runner_1.java
+++ src/test/java/BDD/Task_Runner_1.java
@@ -3,7 +3,7 @@ package BDD;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-import org.testng.annotations.Test
+import org.testng.annotations.Test;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
